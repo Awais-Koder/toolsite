3d6e304966be25d7d286da459cfc64c350d5989d
awais-koder <awaistayyab27@gmail.com>
2026-05-11 15:41:06 +0500

    2026-05-11 15:41:06

--- app/Http/Controllers/SitemapController.php
+++ app/Http/Controllers/SitemapController.php
@@ -6,40 +6,42 @@ class SitemapController extends Controller
 {
     public function index()
     {
+        $lastmod = fn ($view) => date(DATE_ATOM, filemtime(resource_path("views/{$view}.blade.php")));
+
         $urls = [
             [
                 'loc' => url('/'),
-                'lastmod' => now()->toAtomString(),
+                'lastmod' => $lastmod('home'),
                 'changefreq' => 'weekly',
                 'priority' => '1.0',
             ],
             [
                 'loc' => route('age-calculator'),
-                'lastmod' => now()->toAtomString(),
+                'lastmod' => $lastmod('age-calculator'),
                 'changefreq' => 'monthly',
                 'priority' => '0.8',
             ],
             [
                 'loc' => route('about'),
-                'lastmod' => now()->toAtomString(),
+                'lastmod' => $lastmod('pages/about'),
                 'changefreq' => 'monthly',
                 'priority' => '0.5',
             ],
             [
                 'loc' => route('privacy'),
-                'lastmod' => now()->toAtomString(),
+                'lastmod' => $lastmod('pages/privacy'),
                 'changefreq' => 'monthly',
                 'priority' => '0.3',
             ],
             [
                 'loc' => route('terms'),
-                'lastmod' => now()->toAtomString(),
+                'lastmod' => $lastmod('pages/terms'),
                 'changefreq' => 'monthly',
                 'priority' => '0.3',
             ],
             [
                 'loc' => route('contact'),
-                'lastmod' => now()->toAtomString(),
+                'lastmod' => $lastmod('pages/contact'),
                 'changefreq' => 'monthly',
                 'priority' => '0.5',
             ],
